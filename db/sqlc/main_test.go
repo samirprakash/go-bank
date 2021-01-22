b9b9b4130184835fcc9f6fa5c6c8d50e91aaa56b
Samir Prakash <Samir.Prakash@gmail.com>
2021-01-22 17:54:10 -0600

    universal testing setup with pq and testify

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to database", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/samirprakash/go-bank
 
 go 1.15
+
+require (
+	github.com/lib/pq v1.9.0
+	github.com/stretchr/testify v1.7.0
+)
