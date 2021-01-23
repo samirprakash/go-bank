ecc9a145e138d23652efd9f55be80ee1087b4210
Samir Prakash <Samir.Prakash@gmail.com>
2021-01-22 19:58:22 -0600

    added tests for GetAccount

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwnerName(),
 		Balance:  util.RandomAmount(),
@@ -25,4 +25,25 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.NotZero(t, account2.ID)
+	require.NotZero(t, account2.CreatedAt)
 }
