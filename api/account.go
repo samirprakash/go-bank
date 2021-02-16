23101e14963964863f7efac765ebc30e8b7d81b8
Samir Prakash <Samir.Prakash@gmail.com>
2021-02-15 21:35:09 -0600

    return proper error code - 403

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "github.com/samirprakash/go-bank/db/sqlc"
 )
 
@@ -33,6 +34,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	// save to db
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
