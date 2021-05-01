d73e6b110e26315777e8806da8737f6f5b709588
Samir Prakash <Samir.Prakash@gmail.com>
2021-04-30 19:29:48 -0500

    added token package

--- go.mod
+++ go.mod
@@ -1,12 +1,13 @@
 module github.com/samirprakash/go-bank
 
-go 1.15
+go 1.16
 
 require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.2.0
 	github.com/golang/mock v1.4.4
+	github.com/google/uuid v1.2.0
 	github.com/lib/pq v1.9.0
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/spf13/viper v1.7.1
---
+++ token/payload.go
@@ -0,0 +1,33 @@
+package token
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Payload contains the payload data for the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, err
+}
