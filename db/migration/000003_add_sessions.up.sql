debee2a9201ed6a9e648f1f9752ba218c8b735f5
Samir Prakash <Samir.Prakash@gmail.com>
2022-03-22 13:02:53 -0500

    add renew access token end point

--- db/migration/000003_add_sessions.up.sql
+++ db/migration/000003_add_sessions.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE "sessions" (
   "username" varchar NOT NULL,
   "refresh_token" varchar NOT NULL,
   "user_agent" varchar NOT NULL,
-  "client_ip" varchar UNIQUE NOT NULL,
+  "client_ip" varchar NOT NULL,
   "is_blocked" boolean NOT NULL DEFAULT false,
   "expires_at" timestamptz NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
